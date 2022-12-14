5a9907703cdd558878ac4eeac7c2b554c3946f6e
Jody <jodywhitis0407@gmail.com>
2022-12-13 22:26:30 -0600

    updating comments, extra extra html changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,94 @@
-#![allow(non_snake_case)] //compiler to ignore warnings during build (project name was not snake cased)
+#![allow(non_snake_case)] //Compiler to ignore warnings during build (project name was not snake cased)
 
-use std::{ //Bring in namespaces of Rust's modules into scope for the class.
-    //The other namespace "sync" and "thread" both are sub modules of std, a group of namespaces are used instead as a short hand notation.
+use std::{ //Bring in namespaces of Rust's modules and standard library crates into scope for the class.
+    //The other namespace "sync" and "thread" both are sub modules of std, a group of namespaces used instead as a short hand notation.
     //(instead of three or more different use statements).
-    sync::{mpsc, Arc, Mutex}, //this module has three other sub-modules, bring these into scope in one statement.
+    sync::{mpsc, Arc, Mutex}, //This module has three other sub-modules, bring these into scope in one statement.
     //These modules help synchronize multi-thread operations that can access and change mutable object values from more than one thread.
-    //mpsc: multi-producer, single consumer first in first out queue. Provides communication over channels. Sets up only one consumer or receiver with multiple callers.(multiple threads with a single destination).
+    //mpsc: Multi-producer, single consumer first in first out queue. Provides communication over channels. Sets up only one consumer or receiver with multiple callers.(multiple requests with a single destination).
     //Arc: atomically ref-counted pointer, used in multi-threaded apps to extend the lifetime of data until all threads are finished with it. Ensures thread safe operations.
     //Mutex: mutual exclusive lock to make sure no more than one thread accesses or alters data of an object. Only one thread at a time on data.
-    thread,//thread: use of thread each with its own stack and local saved state. This brings in the thread type along with its operations.
+    thread, //thread: use of thread each with its own stack and local saved state. This brings in the thread type along with its operations.
 };
 
-pub struct ThreadPool{ //an object to have a pool of spawned threads. This will hold workers to perform work in the thread along with the sending of the job.
-    workers: Vec<Worker>, //holds vector of Worker structure that will store threads to perform operations.
+pub struct ThreadPool{ //An object to have a pool of spawned threads. This will hold workers to perform work in the thread along with the sending of the job.
+    workers: Vec<Worker>, //Holds vector of Worker structure that will store threads to perform operations.
     sender: Option<mpsc::Sender<Job>>, //Create a channel that will hold the sender of a Job that will be sent through the channel.
+//Option either returns a object of Job to be sent with the Sender in a asynchronous channel (Some) or None (nothing to send)
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>; //type alias to hold a pointer to a heap.
+type Job = Box<dyn FnOnce() + Send + 'static>; //Type alias to hold a pointer to a heap and a function pointer.
 // A vary amount of jobs get saved in the heap as we may not know the exact amount of jobs/threads that are performing operations.
 // We'll get a closure to be executed once as each job in a thread runs once before the thread is returned to the pool.
 
 impl ThreadPool{ //The implementation of the pool that will hold sending, receiving and workers for each thread.
-    pub fn new(size: usize) -> ThreadPool{ //type of constructor that take a parameter of size (the number of max threads).
-        assert!(size > 0); //error handling macro if to validate we have at least one thread running from the pool.
+    pub fn new(size: usize) -> ThreadPool{ //Type of constructor that take a parameter of size (the number of max threads).
+        assert!(size > 0); //Error handling macro to validate we have at least one thread running from the pool.
 
-        let (sender,receiver)  = mpsc::channel(); //tuple of an async channel of a sender and receiver of both will hold a Job structure.
-        let receiver = Arc::new(Mutex::new(receiver)); //assign a thread-safe, mutual exclusive lock of the receiver.
+        let (sender,receiver)  = mpsc::channel(); //Tuple of an async channel of a sender and receiver of both will hold a Job structure.
+        let receiver = Arc::new(Mutex::new(receiver)); //Assign a thread-safe, mutual exclusive lock of the receiver from the channel.
         //This ensure will be able to lock the receiver, keeping only one thread accessing data from this data at a time.
-    // The lock as to be acquire then release for each thread to encourage atomicity on a receiver job.
-    //This lets multiple worker owner to the receiver while the Mutex has only one worker gets a Job structure at a time.
-        let mut workers = Vec::with_capacity(size); //create an mutable worker Vec that will be created according to the ThreadPool size parameter.
+        //The lock as to be acquire then release for each thread to encourage atomicity on a receiver job.
+        //This lets multiple worker owner to the receiver while the Mutex has only one worker gets a Job structure at a time.
+        let mut workers = Vec::with_capacity(size); //Creates an mutable worker Vec that will be created according to the ThreadPool size parameter.
 
-        for id in 0..size{ //loop through the available threads.
-            workers.push(Worker::new(id,Arc::clone(&receiver))); //add worker instance for a thread from a cloned thread-safe reference to the receiver.
-            // Each worker will need a reference to the receiver to get back the response, thread-safe Arc for multiple threads receiving the single response message asynchronously.
+        for id in 0..size{ //Loop through the available threads.
+            workers.push(Worker::new(id,Arc::clone(&receiver))); //Add worker instance for a thread from a cloned thread-safe reference to the receiver.
+            //Each worker will need a reference to the receiver to get back the response, thread-safe Arc for multiple threads receiving the single response message asynchronously.
         }
-        ThreadPool{ //create an instance of ThreadPool with workers and a sender wrapped in Some.
-            workers, //store JoinHandler to take closure of logic to run through and send it back to the existing thread.
-            sender: Some(sender), //sender data to write back to the stream.
+        ThreadPool{ //Creates an instance of ThreadPool with workers and a sender wrapped in Some.
+            workers, //Store JoinHandler to take closure of logic to run through and send it back to the existing thread.
+            sender: Some(sender), //Sender data to write back to the stream.
         }
     }
-    pub fn execute<F>(&self, f: F) //execute function with a self reference to itself (the thread).
-    where //below executes based on the condition
-    F: FnOnce() + Send + 'static, //type alias that can be called only once with an unknown static lifetime scope the thread.
-    //Send takes ownership and executes once, this trait bound transfers closure to threads. This captures the environment variables.
+    pub fn execute<F>(&self, f: F) //Execute function with a self reference to itself (the thread).
+    where //Below executes based on the condition
+    F: FnOnce() + Send + 'static, //Type alias that can be called only once with an unknown static lifetime scope the thread.
+    //Send takes ownership and executes once, this trait bound transfers closure to threads with threads borrowing data to use.
     {
-        let job = Box::new(f); //smart pointer to allocates memory in the heap and places f type Job alias there.
-        self.sender.as_ref().unwrap().send(job).unwrap(); //sends the job through the channel
+        let job = Box::new(f); //Smart pointer to allocates memory in the heap and places f type Job alias there.
+        self.sender.as_ref().unwrap().send(job).unwrap(); //Sends the job through the channel.
     }
 }
 
-impl Drop for ThreadPool{ //implementing a destructor for the ThreadPool
-    fn drop(&mut self){ //take mutable pointer if itself to implement drop.
-        drop(self.sender.take()); //take sender out of object to remove from memory, leave None after dropping.
-    //this explicitly drops sender out of the pool before waiting for threads to finish, this will address the main thread blocking indefinitely on a endless loop preventing deadlocks and starvation.
+impl Drop for ThreadPool{ //Implementing a type of memory allocator for the ThreadPool to remove from memory.
+    fn drop(&mut self){ //Take mutable reference if itself to implement drop.
+        drop(self.sender.take()); //Take sender out of object to remove from memory, leave None after dropping.
+    //This explicitly drops sender out of the pool before waiting for threads to finish.
+    //This will address the main thread blocking indefinitely on a endless loop preventing deadlocks and starvation.
 
-        for worker in &mut self.workers{ //loop through workers from a mutable reference of the workers being dropped
-            println!("Shutting down worker {}", worker.id); //print out the current worker id
+        for worker in &mut self.workers{ //Loop through workers from a mutable reference of the workers being dropped.
+            println!("Shutting down worker {}", worker.id); //Print out the current worker id.
 
-            if let Some(thread) = worker.thread.take(){ //if the thread has a worker
-                thread.join().unwrap(); //wait for that thread to finish its operation before dropping
+            if let Some(thread) = worker.thread.take(){ //If the thread has a worker, take the mutable reference and bind to a conditional statement.
+                thread.join().unwrap(); //Wait for that thread to finish its operation before dropping out of scope
             }
         }
     }
 }
 struct Worker{ //Worker object that holds an ID and a thread
-    id: usize, //id to keep track of a worker performing with a thread.
-    thread: Option<thread::JoinHandle<()>>, //the thread the worker is working on, used as optional to return Some or none.
+    id: usize, //Id to keep track of a worker performing with a thread.
+    thread: Option<thread::JoinHandle<()>>, //The thread a worker is working on, used as optional to return Some or none.
 }
 
 impl Worker{ //implemented operations of the worker object
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{ //initialize a new worker with an id and receiver field that is thread safe mutual exclusive lock to ensure atomicity on thread work in this reciever.
-        let thread = thread::spawn(move||loop{ //spawn a thread, move it in the closure and force ownership of new threads with the environment variables.
-           let message = receiver.lock().unwrap().recv(); //lock and unwraps to receive a message, this a result of a job and possible error in receiving.
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{ //Initialize a new worker with an id and receiver field that is thread safe mutual exclusive lock to ensure atomicity on thread work in this receiver.
+        let thread = thread::spawn(move||loop{ //Spawn a thread, move it in the closure and force ownership of receiver with the environment variables.
+           let message = receiver.lock().unwrap().recv(); //Lock and unwraps to receive a message, this a result of a job and possible error in receiving.
+            //Since we force ownership, we lock to make sure no other threads are effecting the receiver object while the Worker's operates.
 
-            match message{ //on condition of the message
-                Ok(job) => { //if its a valid job and it is initiated then print to the console.
-                    println!("Worker {id} got a job; executing."); //print the id of the worker that is executing the job.
+            match message{ //On condition of the message (the Job object)
+                Ok(job) => { //If its a valid job and it is initiated then print to the console.
+                    println!("Worker {id} got a job; executing."); //Print the id of the worker that is executing the job.
                     job();
                 }
-                Err(_) => { //on any error, print the disconnection message to the console. This say the worker is done and is being dropped.
-                    println!("Worker {id} disconnected; shutting down."); //print the id of the worker that is being dropped.
-                    break; //terminate the execution of the loop.
+                Err(_) => { //On any error, print the disconnection message to the console. This say the worker is done and is being dropped.
+                    println!("Worker {id} disconnected; shutting down."); //Print the id of the worker that is being dropped.
+                    break; //Terminate the execution of the loop.
                 }
             }
 
         });
-        Worker{id, thread: Some(thread),} //worker object with its id field and thread the worker would perform the job in.
+        Worker{id, thread: Some(thread),} //Worker object with its ID field and thread the worker would perform the job in.
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,47 @@
-#![allow(non_snake_case)] //tell the compiler to ignore snake case warning(project was not named in snake case)
+#![allow(non_snake_case)] //Tells the compiler to ignore snake case warning(project was not named in snake case)
 
-use MultiThreadProject::ThreadPool; //bring in ThreadPool object into scope for main.
+use MultiThreadProject::ThreadPool; //This brings in the ThreadPool object into scope for main from the lib.rs file.
 
-use std::{ //bring in three sub-modules into scope along with its sub-modules/objects.
-    fs, //file operations, used to read html scripts that will be used as responses.
-    io::{prelude::*, BufReader}, //io operations with two sub-modules. Used to write and read streams using in communicating through channels.
-    net::{TcpListener,TcpStream}, //net operations that add TCP listeners for incoming request and the stream that the requests/response are traveling through.
+use std::{ //This brings in three sub-modules into scope along with its sub-modules/objects.
+    fs, //File operations functions, used to read HTML scripts that will be used as responses.
+    io::{prelude::*, BufReader}, //IO operations with two sub-modules. Used to write and read streams using in communicating through channels.
+    net::{TcpListener,TcpStream}, //Net operations that add TCP listeners for incoming request and the stream that the requests/response are traveling through.
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); //create a listener instance binding at this address/port while unwrapping any possible errors. Returns a Result<T,E>.
-    let pool = ThreadPool::new(4); //create a thread pool holding 4 max threads.
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap(); //Create a listener instance that is bounded to this address and binds to the port while unwrapping any possible errors. Returns a Result<T,E>.
+    let pool = ThreadPool::new(4); //Creates a thread pool holding 4 max threads. The max limit passed in the constructor parameter.
 
-    for stream in listener.incoming().take(2){ //take in 2 request that will come from the address and shut down after the second thread completes its operation.
-        //This request will be listening at this address/port and loop through the requests with a defined limit to shut down and dispose of the stream automatically.
-        let stream = stream.unwrap(); //unwraps a Result of TCP Stream and takes the stream or the error associated with it.
+    for stream in listener.incoming().take(2){ //Take in 2 requests that will come from the address and shut down after the second thread completes its operation.
+        //This request will be listening at this address/port and loop through the requests with a defined limit to shut down and dispose of the stream automatically at the end of take(n).
+        let stream = stream.unwrap(); //Unwraps a result of TCP Stream and takes the stream or the error associated with it.
 
-        pool.execute(||{ //execute the thread's operations by taking the closure this is implement and define within the ThreadPool data structure.
-            handle_connection(stream); //pass in a mutable stream to the function.
-        });
+        pool.execute(||{ //Execute the thread's operations by taking the closure that is implement and define within the ThreadPool data structure.
+            handle_connection(stream); //The closure will capture the stream as part of the environment, borrowing the stream to pass in the function to handle write operations.
+         });
       }
-    println!("Shutting down."); //listener is finished looping incoming streams, stream is shut down and dropped from scope while the listener is not longer taking in connections.
+    println!("Shutting down."); //The listener is finished looping through incoming streams, stream is shut down and dropped from scope while the listener is not longer taking in connections.
 }
 
-fn handle_connection(mut stream: TcpStream){ //connection handler that will take a mutable reference to the tcp stream.
-    let buf_reader = BufReader::new(&mut stream); //create a reader from a mutable pointer of stream to read the incoming data in the stream.
-    let request_line = buf_reader.lines().next().unwrap().unwrap(); //iterates over the lines and returns a string and unwraps errors at lines() and next() if any occur to panic the application.
+fn handle_connection(mut stream: TcpStream){ //Connection handler that will take a mutable reference to the TCP stream.
+    let buf_reader = BufReader::new(&mut stream); //Creates a reader from a mutable reference of stream to read the incoming data in the stream.
+    //This borrows the stream to be mutable, that is needed when writing to the stream for different get requests.
+    let request_line = buf_reader.lines().next().unwrap().unwrap(); //Iterates over the lines, returns a string and unwraps errors at lines() and next(). If any errors occur to panic the application.
 
 
-    let(status_line, filename) = if request_line == "GET / HTTP/1.1"{ //create a http get request with its parameters.
-        // Assign a tuple holding the status HTTP along with the filename of the html script to send down the response stream.
-        ("HTTP/1.1 200 OK", "hello.html") //send status code and http version along with the body contents.
+    let(status_line, filename) = if request_line == "GET / HTTP/1.1"{ //Create a HTTP GET request with its parameters.
+        // Assign a tuple holding the status HTTP (message and code) along with the filename of the HTML script to send down the response stream.
+        ("HTTP/1.1 200 OK", "hello.html") //Send status code and HTTP version along with the body contents.
     }else{
-        ("HTTP/1.1 404 NOT FOUND","404.html") //send a 404 status code and an error html body.
+        ("HTTP/1.1 404 NOT FOUND","404.html") //Send a 404 status code and an error HTML body.
     };
 
-    let contents = fs::read_to_string(filename).unwrap(); //read a file to a string and unwraps errors to panic the application.
-    let length = contents.len(); //get the length of the html body. This is a parameter in the HTTP get request.
+    let contents = fs::read_to_string(filename).unwrap(); //Read a file to a string and unwraps errors to panic the application.
+    let length = contents.len(); //Gets the length of the HTML body. This is a parameter in the HTTP GET request.
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"); //create the response string to send down the stream.
-    //The http requests consists of parameters: status line (status code, http version, and status message, content-length, and the string contents separated by newlines.
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"); //Create the response string to send down the stream.
+    //The HTTP requests consists of parameters: status line (status code, http version, and status message), content-length, and the string contents separated by new lines.
 
-    stream.write_all(response.as_bytes()).unwrap(); //write bytes to the stream to send back, unwrap errors.
+    stream.write_all(response.as_bytes()).unwrap(); //Write bytes to the stream to send back, unwrap errors.
 
 }
